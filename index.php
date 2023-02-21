4f3f85355ad60f3f2805acce3c38cbf9a3ff6651
1W3UxNt <120009117+1W3UxNt@users.noreply.github.com>
2023-02-21 12:45:21 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+    
+$showAlert = false; 
+$showError = false; 
+$exists=false;
+    
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+    // Include file which makes the
+    // Database Connection.
+    include 'dbconnect.php';   
+    
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+    $cpassword = $_POST["cpassword"];
+            
+    
+    $sql = "Select * from users where username='$username'";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    $num = mysqli_num_rows($result); 
+    
+    
+    // Этот sql-запрос используется для проверки
+    // имя пользователя уже присутствует
+    // или нет в нашей базе данных
+    if($num == 0) {
+        if(($password == $cpassword) && $exists==false) {
+    
+            $hash = password_hash($password, 
+                                PASSWORD_DEFAULT);
+                
+            // Password Hashing is used here. 
+            $sql = "INSERT INTO `users` ( `username`, 
+                `password`, `date`) VALUES ('$username', 
+                '$hash', current_timestamp())";
+    
+            $result = mysqli_query($conn, $sql);
+    
+            if ($result) {
+                $showAlert = true; 
+            }
+        } 
+        else { 
+            $showError = "Passwords do not match"; 
+        }      
+    }
+    
+   if($num>0) 
+   {
+      $exists="Username not available"; 
+   } 
+    
+}//end if   
+    
+?>
+
