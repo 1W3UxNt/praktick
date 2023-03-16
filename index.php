5f1ba769b02820b9ec3167f47665c072a10047fd
1W3UxNt <120009117+1W3UxNt@users.noreply.github.com>
2023-03-16 12:32:51 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <?php
-$host= 'localhost';
+$host = 'localhost';
 $user = 'root';
 $pass = '';
 $db = 'form';
-  
-$conn = mysqli_connect($host , $user , $pass , $db);
-if($conn === false){
-  die("ERROR: Could not connect. "
-      . mysqli_connect_error());
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if ($conn === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$mail = mysqli_real_escape_string($conn,$_POST['mail']);
-$password = mysqli_real_escape_string($conn,$_POST['psw']);
-$password = md5($_POST['password']);
-$sql = "INSERT INTO form (email, password)
-VALUES ('$mail', '$password' )";
+// Использование подготовленных выражений
+$stmt = mysqli_prepare($conn, "INSERT INTO form (email, password) VALUES (?, ?)");
+if ($stmt === false) {
+    die("ERROR: Could not prepare statement. " . mysqli_error($conn));
+}
 
-if(mysqli_query($conn, $sql)){
-  mysqli_close($conn);
-  header("Location: index.html");
-  exit;
-} 
-else{
-  echo "Ошибка $sql. "
-      . mysqli_error($conn);
-  mysqli_close($conn);
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+
+// Использование более сильного алгоритма хэширования
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+mysqli_stmt_bind_param($stmt, 'ss', $mail, $hashed_password);
+
+if (mysqli_stmt_execute($stmt)) {
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+    echo '<script>alert("Информация добавлена успешно."); window.location.href="index.html";</script>';
+    exit;
+} else {
+    echo "Ошибка: " . mysqli_error($conn);
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 }
 ?>
