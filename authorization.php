ca6a99039c7fa02b22e06ef4133106479060f2d1
1W3UxNt <120009117+1W3UxNt@users.noreply.github.com>
2023-03-31 09:51:20 +0600

    Update authorization.php

--- authorization.php
+++ authorization.php
@@ -10,7 +10,7 @@ if ($conn === false) {
 }
 
 $mail = $_POST['mail'];
-$password = $_POST['psw'];
+$password = md5($_POST['psw']);
 
 // Использование подготовленных выражений
 $stmt = mysqli_prepare($conn, "SELECT * FROM form WHERE email=? AND password=?");
@@ -19,16 +19,9 @@ mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 if (mysqli_num_rows($result) == 1) {
     $row = mysqli_fetch_assoc($result);
-    if (password_verify($password, $row['password'])) {
-        // Если пароль верен, выполняется авторизация
-        session_start();
-        $_SESSION['mail'] = $mail;
-        header('Location: welcome.php');
-        exit;
-    } else {
-        // Если пароль неверен, выводится сообщение об ошибке
-        echo '<script>alert("Неверный пароль."); window.location.href="authorization.html";</script>';
-    }
+    session_start();
+    $_SESSION['login'] = true;
+    header("Location: welcome.php");
 } else {
     // Если пользователь с таким email не найден, выводится сообщение об ошибке
     echo '<script>alert("Неверный логин."); window.location.href="authorization.html";</script>';
